8ffa84c504c87ed6684369626888b4a02ac20b62
gtom84 <53334241+gtom84@users.noreply.github.com>
2021-02-06 21:54:15 +0100

    Create ajax.php

---
+++ ajax.php
@@ -0,0 +1,13 @@
+<?php
+
+  $token = $_REQUEST["id"];
+
+
+	/*
+		Connect to WP database, get data (column jidlo) for scanned token.
+		Check if individual payed for this meal and return response.
+	
+	*/
+
+
+?>
